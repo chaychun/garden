f7fbc5ef8e12c28e2c18c90ba07b77ed9603023f
Chayut <chun.chayut@gmail.com>
2025-07-20 13:36:38 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		react({
+			babel: {
+				plugins: [["babel-plugin-react-compiler"]],
+			},
+		}),
+	],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import tsParser from "@typescript-eslint/parser";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginAstro from "eslint-plugin-astro";
+import reactCompiler from "eslint-plugin-react-compiler";
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	...eslintPluginAstro.configs.recommended,
+	eslintConfigPrettier,
+	{
+		files: ["**/*.{ts,tsx}"],
+		ignores: [".vercel/**", "dist/**", "build/**"],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+		},
+		plugins: {
+			"react-compiler": reactCompiler,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			...reactCompiler.configs.recommended.rules,
+		},
+	},
+]);
