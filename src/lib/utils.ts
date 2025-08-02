4e1eb2bc79f1c2c0ad3de1df055251f89ee29a80
Chayut <chun.chayut@gmail.com>
2025-08-02 14:48:32 +0700

    Metadata table

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function formatDate(dateString: string): string {
+	const date = new Date(dateString);
+	const day = date.getDate();
+	const month = date.toLocaleDateString("en-US", { month: "short" });
+	const year = date.getFullYear();
+
+	return `${day} ${month}, ${year}`;
+}
