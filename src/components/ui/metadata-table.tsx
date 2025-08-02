856bd0af089eb2e00ba69e7e1f38e5d77efc5654
Chayut <chun.chayut@gmail.com>
2025-08-02 20:28:57 +0700

    Full page layout (#25)
    * Metadata table
    
    * Preview block link to full page
    
    * Full page layout
    
    * Content area script improvement
    
    * Add shadcn scroll-area & allow sidebar to scroll vertically

---
+++ src/components/ui/metadata-table.tsx
@@ -0,0 +1,70 @@
+import { cn, formatDate } from "@/lib/utils";
+
+interface Props {
+	createdDate: string;
+	lastUpdatedDate: string;
+	technologies: string[];
+	className?: string;
+}
+
+const MetadataTable = ({
+	createdDate,
+	lastUpdatedDate,
+	technologies,
+	className,
+}: Props) => {
+	return (
+		<div className={cn("w-full", className)}>
+			{/* Title */}
+			<div className="text-base-500 mb-2 text-xs font-medium tracking-wide uppercase">
+				DETAILS
+			</div>
+
+			{/* Horizontal separator */}
+			<div className="flex">
+				<div className="border-base-100 h-2 w-full flex-1 border-x border-t" />
+				<div className="border-base-100 h-2 w-full flex-1 border-t border-r" />
+			</div>
+
+			{/* Dates */}
+			<div className="mt-1 flex">
+				<div className="flex-1">
+					<div className="text-base-400 text-[10px] tracking-wide">Created</div>
+					<div className="text-base-700 font-mono text-xs">
+						{formatDate(createdDate)}
+					</div>
+				</div>
+				<div className="flex-1">
+					<div className="text-base-400 text-[10px] tracking-wide">
+						Last Updated
+					</div>
+					<div className="text-base-700 font-mono text-xs">
+						{formatDate(lastUpdatedDate)}
+					</div>
+				</div>
+			</div>
+
+			{/* Horizontal separator */}
+			<div className="border-base-100 mt-4 h-2 w-full border-x border-t" />
+
+			{/* Technologies */}
+			<div className="mt-1">
+				<div className="text-base-400 mb-1 text-[10px] tracking-wide">
+					Technologies
+				</div>
+				<div className="flex flex-wrap gap-1">
+					{technologies.map((tech) => (
+						<span
+							key={tech}
+							className="border-base-200/30 text-base-500 hover:bg-base-200/30 inline-block border px-3 py-1 font-mono text-[10px] transition-colors duration-200 select-none hover:border-transparent"
+						>
+							{tech}
+						</span>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default MetadataTable;
