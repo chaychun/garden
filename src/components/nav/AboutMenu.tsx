8c05022998b3d666fae4042081c26ffe8fb4ebc2
Chayut <chun.chayut@gmail.com>
2025-08-11 22:11:01 +0700

    Fixes

--- src/components/nav/AboutMenu.tsx
+++ src/components/nav/AboutMenu.tsx
@@ -28,6 +28,8 @@ export default function AboutMenu({ isOpen }: AboutMenuProps) {
 
 	return (
 		<motion.div
+			role="dialog"
+			aria-modal="true"
 			aria-hidden={!isOpen}
 			initial={false}
 			animate={{
@@ -44,7 +46,7 @@ export default function AboutMenu({ isOpen }: AboutMenuProps) {
 				<h1 className="text-base-900 text-5xl font-semibold tracking-tight md:text-7xl">
 					Chayut Chunsamphran
 				</h1>
-				<div className="mt-16 w-full flex-1 md:flex md:gap-12">
+				<div className="mt-16 w-full flex-1 md:flex">
 					<div className="flex flex-col gap-16 md:flex-2 md:justify-between">
 						<p className="text-base-900 md:clamp-[text,24px,40px,md,2xl] text-[28px] leading-none">
 							I build thoughtful interactive websites and web apps that enhance
@@ -56,14 +58,15 @@ export default function AboutMenu({ isOpen }: AboutMenuProps) {
 							src={profile}
 							alt="Chayut's portrait"
 							className="w-3/4 max-w-[400px]"
+							loading="lazy"
 						/>
 					</div>
 					<div className="mt-16 flex flex-col md:mt-0 md:flex-3 md:justify-between">
-						<div className="grid grid-cols-4 gap-y-4">
-							<div className="text-base-500 col-start-1 font-mono text-xs md:text-sm">
+						<div className="grid grid-cols-4 gap-y-4 md:grid-cols-3">
+							<div className="text-base-500 col-start-1 font-mono text-xs md:mr-12 md:justify-self-end md:text-sm">
 								Vision
 							</div>
-							<div className="text-base-900 col-span-3 col-start-2 space-y-2 leading-none md:text-xl">
+							<div className="text-base-900 md:clamp-[text,lg,2xl,md,2xl] col-span-3 col-start-2 space-y-2 leading-none!">
 								<p>
 									I believe technology should adapt to our needs, supporting the
 									way we think, work, and live. With the right patterns, it can
@@ -81,10 +84,10 @@ export default function AboutMenu({ isOpen }: AboutMenuProps) {
 								</p>
 							</div>
 
-							<div className="text-base-500 col-start-1 row-start-3 font-mono text-xs md:text-sm">
+							<div className="text-base-500 col-start-1 row-start-3 font-mono text-xs md:mr-12 md:justify-self-end md:text-sm">
 								Details
 							</div>
-							<p className="text-base-900 col-span-3 col-start-2 row-start-3 leading-none md:text-xl">
+							<p className="text-base-900 md:clamp-[text,lg,2xl,md,2xl] col-span-3 col-start-2 row-start-3 leading-none!">
 								I build content-first websites and web apps, working mainly with{" "}
 								<UnderlineLink
 									href="https://astro.build"
